827616e9c3a9e4420b8f5c4b71418be5738dceef
Vasilen Tsvetkov <vasilen@7digit.io>
2022-11-26 21:32:48 +0000

    Refactor: rename column "living_place" to "urban_rural"

--- service/queries/clients.js
+++ service/queries/clients.js
@@ -3,7 +3,7 @@ import { getDBPool } from "#utils/dbConfig";
 export const getClientByIdQuery = async ({ poolCountry, clientId }) =>
   await getDBPool("piiDb", poolCountry).query(
     `
-      SELECT client_detail."client_detail_id", "name", surname, nickname, email, image, sex, year_of_birth, living_place, data_processing, access_token
+      SELECT client_detail."client_detail_id", "name", surname, nickname, email, image, sex, year_of_birth, urban_rural, data_processing, access_token
       FROM client_detail
         JOIN "user" ON "user".client_detail_id = client_detail.client_detail_id AND "user".deleted_at IS NULL
       WHERE client_detail.client_detail_id = $1
