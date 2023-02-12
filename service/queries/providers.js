c1a3f211b02b0acb0ec5101d1ee819b4cade003b
Georgi 7DIGIT <georgi@7digit.io>
2023-02-12 22:35:20 +0200

    Fix: rename missing column

--- service/queries/providers.js
+++ service/queries/providers.js
@@ -324,7 +324,7 @@ export const deleteProviderImageQuery = async ({ poolCountry, provider_id }) =>
 export const getActivitiesQuery = async ({ poolCountry, providerId }) => {
   return await getDBPool("clinicalDb", poolCountry).query(
     `
-      SELECT client_detail_id, provider_detail_id, time, status, price, type, coupon_id FROM consultation
+      SELECT client_detail_id, provider_detail_id, time, status, price, type, campaign_id FROM consultation
         INNER JOIN transaction_log ON consultation.consultation_id = transaction_log.consultation_id
       WHERE provider_detail_id = $1 AND (status = 'finished' OR (status = 'scheduled' AND now() > time + interval '1 hour'))
     `,
