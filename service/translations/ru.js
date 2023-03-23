1da0d95e1a2fa92205d58413a9fe5742d4136ba2
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2023-03-24 00:51:51 +0200

    Add: russian and kazakh translations

---
+++ service/translations/ru.js
@@ -0,0 +1,16 @@
+export default {
+  provider_not_found_error: "Консультант не найден",
+  client_not_found_error: "Клиент не найден",
+  incorrect_password_error: "Предоставленный пароль неверен",
+  email_already_used_error: "Этот электронный адрес уже используется",
+  slots_not_within_week_error: "Временной промежуток выходит за рамки недели",
+  slot_not_available_error: "Время недоступно",
+  consultation_not_found_error: "Консультация не найдена",
+  provider_has_future_consultations_error: "У консультанта есть будущие консультации",
+  user_not_found_error: "Пользователь не найден",
+  consultation_not_scheduled_error: "Консультация не запланирована",
+  service_not_found_error: "Сервис не найден",
+  transaction_not_found_error: "Transaction not found",
+  campaign_not_found_error: "Campaign not found",
+  slot_already_exists_error: "Slot already marked as available",
+};
