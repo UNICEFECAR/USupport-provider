b0f94e401ec471d3427216a81d6b18b580270a10
Georgi 7DIGIT <georgi@7digit.io>
2024-09-26 23:09:50 +0300

    Fix: add organization_slots to the array of normal slots in getCalendarFiveWeeks

--- service/controllers/calendar.js
+++ service/controllers/calendar.js
@@ -23,9 +23,13 @@ export const getCalendarFiveWeeks = async ({
   }).catch((err) => {
     throw err;
   });
+  console.log(slotsData);
 
+  const organizationSlotTimes = Array.isArray(slotsData.organization_slots)
+    ? slotsData.organization_slots.map((x) => x.time)
+    : [];
   return {
-    slots: slotsData.slots,
+    slots: [...slotsData.slots, ...organizationSlotTimes],
     campaign_slots: slotsData.campaign_slots,
     campaigns_data: slotsData.campaigns_data,
     consultations,
