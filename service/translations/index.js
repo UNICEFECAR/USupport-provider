e8e53af59452e37796fd8292092170a5420f4e92
Stefan Dobrev <stefandobrev88@gmail.com>
2025-08-12 16:03:08 +0300

    Add: Armenian translations

--- service/translations/index.js
+++ service/translations/index.js
@@ -1,10 +1,12 @@
-import en from "./en.js";
-import ru from "./ru.js";
-import kk from "./kk.js";
-import pl from "./pl.js";
-import uk from "./uk.js";
+import am from './am.js';
+import en from './en.js';
+import ru from './ru.js';
+import kk from './kk.js';
+import pl from './pl.js';
+import uk from './uk.js';
 
 const translations = {
+  am,
   en,
   kk,
   ru,
@@ -18,10 +20,10 @@ const translations = {
  * @param {string} language the alpha2 code of the language
  * @returns {string} the translated string
  */
-export function t(key, language = "en") {
+export function t(key, language = 'en') {
   // Make sure the language exists and if not return the default language
   if (!Object.keys(translations).includes(language)) {
-    return translations["en"][key];
+    return translations['en'][key];
   }
   return translations[language][key];
 }
